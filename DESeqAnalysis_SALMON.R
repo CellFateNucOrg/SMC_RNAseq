be0eae443bf9e0e0ff1ac3472717074ddc60368e
Jennifer Semple <semple.j@gmail.com>
2021-04-16 16:15:06 +0200

    ensuring dplyr commands used

--- DESeqAnalysis_SALMON.R
+++ DESeqAnalysis_SALMON.R
@@ -1115,12 +1115,13 @@ includeChrX<-"chrX" %in% unlist(lapply(sigTables,"[","chr"))
 options(tibble.width=Inf)
 dd1<-sig %>% filter(padj<localPadj) %>%
    dplyr::group_by(SMC,upVdown,XvA) %>%
-   mutate(ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange)))
+   dplyr::mutate(ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange)))
 
-ss1<-sig %>% group_by(SMC,XvA) %>% mutate(expOnChr=n()) %>% filter(padj<localPadj) %>%
-   mutate(sigOnChr=n())%>% group_by(SMC,XvA,upVdown) %>%
-   mutate(sigInGrp=n(), ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange))) %>%
-   summarise(expOnChr=unique(expOnChr),
+ss1<-sig %>% dplyr::group_by(SMC,XvA) %>% dplyr::mutate(expOnChr=n()) %>%
+   dplyr::filter(padj<localPadj) %>%
+   dplyr::mutate(sigOnChr=n())%>% dplyr::group_by(SMC,XvA,upVdown) %>%
+   dplyr::mutate(sigInGrp=n(), ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange))) %>%
+   dplyr::summarize(expOnChr=unique(expOnChr),
              sigOnChr=unique(sigOnChr),
              sigInGrp=unique(sigInGrp),
              qnt0=1-ecdf(abs(log2FoldChange))(c(0)),
@@ -1205,11 +1206,12 @@ includeChrX<-"chrX" %in% unlist(lapply(sigTables,"[","chr"))
 options(tibble.width=Inf)
 dd1<-sig %>% filter(padj<localPadj) %>%
    dplyr::group_by(SMC,upVdown,XvA) %>%
-   mutate(ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange)))
+   dplyr::mutate(ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange)))
 
-ss1<-sig %>% group_by(SMC,XvA) %>% mutate(expOnChr=n()) %>% filter(padj<localPadj) %>%
-   mutate(sigOnChr=n())%>% group_by(SMC,XvA,upVdown) %>%
-   mutate(sigInGrp=n(), ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange))) %>%
+ss1<-sig %>% dplyr::group_by(SMC,XvA) %>% dplyr::mutate(expOnChr=n()) %>%
+   dplyr::filter(padj<localPadj) %>%
+   dplyr::mutate(sigOnChr=n())%>% dplyr::group_by(SMC,XvA,upVdown) %>%
+   dplyr::mutate(sigInGrp=n(), ecd=ecdf(abs(log2FoldChange))(abs(log2FoldChange))) %>%
    summarise(expOnChr=unique(expOnChr),
              sigOnChr=unique(sigOnChr),
              sigInGrp=unique(sigInGrp),
