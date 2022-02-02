3baa61be0fcd5192a1c6fc361e9137c80807ea02
jsemple19 <jennifer.semple@unibe.ch>
2022-02-02 13:27:56 +0100

    merging technical replicates script added

--- mergeBWofTechnicalRep.sh
+++ mergeBWofTechnicalRep.sh
@@ -2,14 +2,17 @@
 #SBATCH --mail-user=jennifer.semple@izb.unibe.ch
 #SBATCH --mail-type=end,fail
 #SBATCH --job-name="RNAseq"
-##SBATCH --partition=epyc2
-##SBATCH --qos=job_epyc2
 #SBATCH --time=0-02:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1#2-9,11-83%12
 
 source $CONDA_ACTIVATE RNAseq
+
+WORK_DIR=$PWD
+
+genomeVer=WS275	
+GENOME_DIR=${HOME}/genomeVer/${genomeVer}
+chromSizesFile=$GENOME_DIR/annotation/ws235.chrom.sizes
 
 echo "current date"
 date
@@ -20,27 +23,23 @@ git branch
 echo "current git version"
 git log -1 --format="%H"
 
-# check if paired end or single end
-grep "\sfileName2\s" fastqList.txt
-if [ "$?" == 0  ]
-then
-        isPE=true
-else
-        echo "running single end commands"
-fi
+mkdir -p ${WORK_DIR}/tracks
 
-fastqFileList=./fastqList.txt
-if [ "$isPE" == "true" ]; then
-        fastqFile1s=(`cut -f1 $fastqFileList`)
-        fastqFile2s=(`cut -f2 $fastqFileList`)
-        sampleNames=(`cut -f3 $fastqFileList`)
-        repeatNums=(`cut -f4 $fastqFileList`)
-        laneNums=(`cut -f5 $fastqFileList`)
-else
-        fastqFile1s=(`cut -f1 $fastqFileList`)
-        sampleNames=(`cut -f2 $fastqFileList`)
-        repeatNums=(`cut -f3 $fastqFileList`)
-        laneNums=(`cut -f4 $fastqFileList`)
-fi
+biolRep=( `awk 'NR!=1 && NF>1 {print $3 "_" $4}' fastqList.txt | sort -u` )
+
+for i in ${biolRep[@]}
+do
+	echo $i
+	repFiles=( `ls ${WORK_DIR}/bamSTAR/*_UniqueMultiple.bw | grep $i` )
+
+	bigWigMerge ${repFiles[@]} ${WORK_DIR}/tracks/${i}_STAR_UniqueMultiple.bedGraph
+
+	bedGraphToBigWig ${WORK_DIR}/tracks/${i}_STAR_UniqueMultiple.bedGraph $chromSizesFile ${WORK_DIR}/tracks/${i}_STAR_UniqueMultiple.bw
+
+	if [ -f ${WORK_DIR}/tracks/STAR_${i}.bw ]; then
+		rm ${WORK_DIR}/tracks/${i}_STAR_UniqueMultiple.bedGraph
+	fi
+done
+
 
 
