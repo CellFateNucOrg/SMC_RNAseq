5f428fbf18494a91cb375951acb44118d775ae84
jsemple19 <jennifer.semple@unibe.ch>
2022-02-07 14:30:45 +0100

    server changes

--- mapRNAreads.sh
+++ mapRNAreads.sh
@@ -61,7 +61,7 @@ genomeVer=WS275
 GENOME_DIR=${HOME}/genomeVer/${genomeVer}
 genomeFile=${GENOME_DIR}/sequence/c_elegans.PRJNA13758.${genomeVer}.genomic.fa
 chromSizesFile=$GENOME_DIR/annotation/ws235.chrom.sizes
-
+annotFile_rpt=${GENOME_DIR}/annotation/c_elegans.PRJNA13758.${genomeVer}.annotations_rpt.gtf
 
 mRNAindex=${GENOME_DIR}/sequence/${genomeVer}_mRNA_index
 ncRNAindex=${GENOME_DIR}/sequence/${genomeVer}_ncRNA_index
@@ -76,151 +76,149 @@ WIGtoBW_DIR=${HOME}/mySoftware
 #FASTQ_DIR=`dirname ${fastqFile}`
 baseName=${sampleName}_${repeatNum}_${laneNum}_${libType}
 
-#######################################################
-## get initial read stats                            ##
-#######################################################
+########################################################
+### get initial read stats                            ##
+########################################################
+#
+##run fastqc on sequences
+#mkdir -p ${WORK_DIR}/qc/rawData
+#fastqc ${fastqFile1} -t $nThreads -o ${WORK_DIR}/qc/rawData
+#fastqc ${fastqFile2} -t $nThreads -o ${WORK_DIR}/qc/rawData
+#	
+########################################################
+### trim adaptors with cutadapt                       ##
+########################################################
+#
+## use cutadapt to trim
+#mkdir -p cutadapt
+#cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  -o cutadapt/${baseName}_R1.fastq.gz -p cutadapt/${baseName}_R2.fastq.gz -j $nThreads ${fastqFile1} ${fastqFile2} > ${WORK_DIR}/qc/cutadapt/report_cutadapt_${baseName}.txt
+#
+##redo fastQC on trimmed reads
+#mkdir -p ${WORK_DIR}/qc/cutadapt
+#fastqc cutadapt/${baseName}_R1.fastq.gz -t $nThreads -o ${WORK_DIR}/qc/cutadapt
+#fastqc cutadapt/${baseName}_R2.fastq.gz -t $nThreads -o ${WORK_DIR}/qc/cutadapt
+#
+#
+########################################################
+### Align to genome with STAR                         ##
+########################################################
+#
+## align to genome
+#echo "aligning to genome..."
+#mkdir -p ${WORK_DIR}/bamSTAR
+#STAR --genomeDir ${GENOME_DIR}/sequence  --readFilesIn ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --readFilesCommand zcat --outFileNamePrefix ${WORK_DIR}/bamSTAR/${baseName}_ --runThreadN $nThreads --outSAMmultNmax 1 --alignIntronMax 5000 --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --outWigType wiggle --outWigNorm RPM
+#
+#samtools index ${WORK_DIR}/bamSTAR/${baseName}_Aligned.sortedByCoord.out.bam
+#
+#${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_UniqueMultiple_F.bw
+#${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_UniqueMultiple_R.bw
+#${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_Unique_F.bw
+#${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_Unique_R.bw
+#
+#rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str1.out.wig
+#rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str2.out.wig
+#rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str1.out.wig
+#rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str2.out.wig
+#
+#
+### create bigwig coverage tracks from bam
+##mkdir -p ${WORK_DIR}/tracks
+##Rscript makeCoverageBigwig.R ${WORK_DIR}/bamSTAR/${baseName}.sorted.bam ${WORK_DIR}/tracks/${baseName}_raw.bw raw
+##Rscript makeCoverageBigwig.R ${WORK_DIR}/bamSTAR/${baseName}.sorted.bam ${WORK_DIR}/tracks/${baseName}_rpm.bw rpm
+#
+#
+########################################################
+### Count reads with Salmon                           ##
+########################################################
+#
+## prepare reference genome
+## See separate script indexTxpts4salmon.sh
+#
+######### NOTE: I do not have estimates for --fldMean and --fldSD as i have no access to the bioanalyser files #########
+########  therefore the quantification based on the effective transcript length will be wrong!!!! Not sure how important this ${WIGtoBW_DIR}#
+## quantify mRNA transcripts
+#${SALMON_SING} salmon quant -i ${mRNAindex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz  --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/mRNA/${baseName} --seqBias --gcBias --numBootstraps 100 
+#
+#echo "finished mRNA alignment"
+#
+#
+#if [[ "${mRNAonly}"=="false" ]]
+#    then
+#    
+#    # quantify ncRNA transcripts
+#    ${SALMON_SING} salmon quant -i ${ncRNAindex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/ncRNA/${baseName} --seqBias --gcBias --numBootstraps 100  
+#     
+#    # quantify pseudoRNA transcripts
+#    ${SALMON_SING} salmon quant -i ${pseudoIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/pseudoRNA/${baseName} --seqBias --gcBias --numBootstraps 100  
+#     
+#    # quantify TnRNA transcripts
+#    ${SALMON_SING} salmon quant -i ${tnIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/tnRNA/${baseName} --seqBias --gcBias --numBootstraps 100 
+#    
+#    
+#    kmerSize=15
+#    rptIndex=${GENOME_DIR}/sequence/${genomeVer}_repeats_index_${kmerSize}
+#    # quantify repeat transcripts
+#    ${SALMON_SING} salmon quant -i ${rptIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/rptRNA_${kmerSize}/${baseName} --seqBias --gcBias --numBootstraps 100
+#    
+#    #######################################################
+#    ## Align to genome with STAR                         ##
+#    #######################################################
+#    
+#    
+#    # align to genome
+#    echo "aligning to genome with STAR..."
+#    mkdir -p ${WORK_DIR}/bamSTARrpts
+#    STAR --genomeDir ${GENOME_DIR}/sequence/repeats  --readFilesIn ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --readFilesCommand zcat --outFileNamePrefix ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_ --runThreadN $nThreads --outFilterMultimapNmax 6000 --outSAMmultNmax 1 --outFilterMismatchNmax 3 --alignIntronMax 1 --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --winAnchorMultimapNmax 6000 --outWigType wiggle --outWigNorm RPM 
+#    
+#    samtools index ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Aligned.sortedByCoord.out.bam
+#
+#    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_UniqueMultiple_F.bw
+#    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_UniqueMultiple_R.bw
+#    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Unique_F.bw
+#    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Unique_R.bw
+#    
+#    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str1.out.wig
+#    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str2.out.wig
+#    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str1.out.wig
+#    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str2.out.wig
+#    
+#    # convert sam to bam, sort and index
+#    #samtools view -@ $nThreads -b ${WORK_DIR}/bamSTAR/rpts_${baseName}_Aligned.out.sam -o ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam
+#    #| samtools sort -@ $nThreads -o ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam -
+#    #samtools index -@ $nThreads ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam
+#    #rm ${WORK_DIR}/bamSTAR/${baseName}_Aligned.out.sam
+#    
+#    
+#    ########
+#    # Align to genome with bwa aln
+#    ########
+#    mkdir -p ${WORK_DIR}/bamBWA
+#    echo "aligning $baseName to genome with BWA aln..."
+#    bwa aln -t $nThreads $genomeFile ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}_R1.sai
+#    bwa aln -t $nThreads $genomeFile ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}_R2.sai
+#    bwa sampe $genomeFile ${WORK_DIR}/bamBWA/${baseName}_R1.sai ${WORK_DIR}/bamBWA/${baseName}_R2.sai ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}.sam
+#    
+#    echo "Removing unmapped reads and converting $baseName SAM to BAM..."
+#    samtools view -b -F 4 -@ $nThreads ${WORK_DIR}/bamBWA/${baseName}.sam > ${WORK_DIR}/bamBWA/${baseName}.bam
+#    
+#    rm ${WORK_DIR}/bamBWA/${baseName}.sam
+#    rm ${WORK_DIR}/bamBWA/${baseName}_R1.sai
+#    rm ${WORK_DIR}/bamBWA/${baseName}_R2.sai
+#    
+#
+#    #--additional-attr=gene_name
+#    echo "converting bwa aln sam file to bam and indexing"
+#    samtools sort -T ${WORK_DIR}/bamBWA/${baseName}  -@ $nThreads -o ${WORK_DIR}/bamBWA/${baseName}_sort.bam  ${WORK_DIR}/bamBWA/${baseName}.bam
+#    samtools index -@ $nThreads ${WORK_DIR}/bamBWA/${baseName}_sort.bam
+#    rm ${WORK_DIR}/bamBWA/${baseName}.bam
 
-#run fastqc on sequences
-mkdir -p ${WORK_DIR}/qc/rawData
-fastqc ${fastqFile1} -t $nThreads -o ${WORK_DIR}/qc/rawData
-fastqc ${fastqFile2} -t $nThreads -o ${WORK_DIR}/qc/rawData
-	
-#######################################################
-## trim adaptors with cutadapt                       ##
-#######################################################
-
-# use cutadapt to trim
-mkdir -p cutadapt
-cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  -o cutadapt/${baseName}_R1.fastq.gz -p cutadapt/${baseName}_R2.fastq.gz -j $nThreads ${fastqFile1} ${fastqFile2} > ${WORK_DIR}/qc/cutadapt/report_cutadapt_${baseName}.txt
-
-#redo fastQC on trimmed reads
-mkdir -p ${WORK_DIR}/qc/cutadapt
-fastqc cutadapt/${baseName}_R1.fastq.gz -t $nThreads -o ${WORK_DIR}/qc/cutadapt
-fastqc cutadapt/${baseName}_R2.fastq.gz -t $nThreads -o ${WORK_DIR}/qc/cutadapt
-
-
-#######################################################
-## Align to genome with STAR                         ##
-#######################################################
-
-# align to genome
-echo "aligning to genome..."
-mkdir -p ${WORK_DIR}/bamSTAR
-STAR --genomeDir ${GENOME_DIR}/sequence  --readFilesIn ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --readFilesCommand zcat --outFileNamePrefix ${WORK_DIR}/bamSTAR/${baseName}_ --runThreadN $nThreads --outSAMmultNmax 1 --alignIntronMax 5000 --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --outWigType wiggle --outWigNorm RPM
-
-samtools index ${WORK_DIR}/bamSTAR/${baseName}_Aligned.sortedByCoord.out.bam
-
-${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_UniqueMultiple_F.bw
-${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_UniqueMultiple_R.bw
-${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_Unique_F.bw
-${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTAR/${baseName}_Unique_R.bw
-
-rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str1.out.wig
-rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.UniqueMultiple.str2.out.wig
-rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str1.out.wig
-rm ${WORK_DIR}/bamSTAR/${baseName}_Signal.Unique.str2.out.wig
-
-
-## create bigwig coverage tracks from bam
-#mkdir -p ${WORK_DIR}/tracks
-#Rscript makeCoverageBigwig.R ${WORK_DIR}/bamSTAR/${baseName}.sorted.bam ${WORK_DIR}/tracks/${baseName}_raw.bw raw
-#Rscript makeCoverageBigwig.R ${WORK_DIR}/bamSTAR/${baseName}.sorted.bam ${WORK_DIR}/tracks/${baseName}_rpm.bw rpm
-
-
-#######################################################
-## Count reads with Salmon                           ##
-#######################################################
-
-# prepare reference genome
-# See separate script indexTxpts4salmon.sh
-
-######## NOTE: I do not have estimates for --fldMean and --fldSD as i have no access to the bioanalyser files #########
-#######  therefore the quantification based on the effective transcript length will be wrong!!!! Not sure how important this ${WIGtoBW_DIR}#
-# quantify mRNA transcripts
-${SALMON_SING} salmon quant -i ${mRNAindex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz  --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/mRNA/${baseName} --seqBias --gcBias --numBootstraps 100 
-
-echo "finished mRNA alignment"
-
-
-if [[ "${mRNAonly}"=="false" ]]
-    then
-    
-    # quantify ncRNA transcripts
-    ${SALMON_SING} salmon quant -i ${ncRNAindex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/ncRNA/${baseName} --seqBias --gcBias --numBootstraps 100  
-     
-    # quantify pseudoRNA transcripts
-    ${SALMON_SING} salmon quant -i ${pseudoIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/pseudoRNA/${baseName} --seqBias --gcBias --numBootstraps 100  
-     
-    # quantify TnRNA transcripts
-    ${SALMON_SING} salmon quant -i ${tnIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/tnRNA/${baseName} --seqBias --gcBias --numBootstraps 100 
-    
-    
-    kmerSize=15
-    rptIndex=${GENOME_DIR}/sequence/${genomeVer}_repeats_index_${kmerSize}
-    # quantify repeat transcripts
-    ${SALMON_SING} salmon quant -i ${rptIndex} -l A -1 ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz -2 ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --validateMappings -p ${nThreads} -o ${WORK_DIR}/salmon/rptRNA_${kmerSize}/${baseName} --seqBias --gcBias --numBootstraps 100
-    
-    #######################################################
-    ## Align to genome with STAR                         ##
-    #######################################################
-    
-    
-    # align to genome
-    echo "aligning to genome with STAR..."
-    mkdir -p ${WORK_DIR}/bamSTARrpts
-    STAR --genomeDir ${GENOME_DIR}/sequence/repeats  --readFilesIn ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz --readFilesCommand zcat --outFileNamePrefix ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_ --runThreadN $nThreads --outFilterMultimapNmax 6000 --outSAMmultNmax 1 --outFilterMismatchNmax 3 --alignIntronMax 1 --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --winAnchorMultimapNmax 6000 --outWigType wiggle --outWigNorm RPM 
-    
-    samtools index ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Aligned.sortedByCoord.out.bam
-
-    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_UniqueMultiple_F.bw
-    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_UniqueMultiple_R.bw
-    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str1.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Unique_F.bw
-    ${WIGtoBW_DIR}/wigToBigWig ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str2.out.wig $chromSizesFile ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Unique_R.bw
-    
-    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str1.out.wig
-    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.UniqueMultiple.str2.out.wig
-    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str1.out.wig
-    rm ${WORK_DIR}/bamSTARrpts/rpts_${baseName}_Signal.Unique.str2.out.wig
-    
-    # convert sam to bam, sort and index
-    #samtools view -@ $nThreads -b ${WORK_DIR}/bamSTAR/rpts_${baseName}_Aligned.out.sam -o ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam
-    #| samtools sort -@ $nThreads -o ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam -
-    #samtools index -@ $nThreads ${WORK_DIR}/bamSTAR/rpts_${baseName}.sorted.bam
-    #rm ${WORK_DIR}/bamSTAR/${baseName}_Aligned.out.sam
-    
-    
-    ########
-    # Align to genome with bwa aln
-    ########
-    mkdir -p ${WORK_DIR}/bamBWA
-    echo "aligning $baseName to genome with BWA aln..."
-    bwa aln -t $nThreads $genomeFile ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}_R1.sai
-    bwa aln -t $nThreads $genomeFile ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}_R2.sai
-    bwa sampe $genomeFile ${WORK_DIR}/bamBWA/${baseName}_R1.sai ${WORK_DIR}/bamBWA/${baseName}_R2.sai ${WORK_DIR}/cutadapt/${baseName}_R1.fastq.gz ${WORK_DIR}/cutadapt/${baseName}_R2.fastq.gz > ${WORK_DIR}/bamBWA/${baseName}.sam
-    
-    echo "Removing unmapped reads and converting $baseName SAM to BAM..."
-    samtools view -b -F 4 -@ $nThreads ${WORK_DIR}/bamBWA/${baseName}.sam > ${WORK_DIR}/bamBWA/${baseName}.bam
-    
-    rm ${WORK_DIR}/bamBWA/${baseName}.sam
-    rm ${WORK_DIR}/bamBWA/${baseName}_R1.sai
-    rm ${WORK_DIR}/bamBWA/${baseName}_R2.sai
-    
-
-    #--additional-attr=gene_name
-    echo "converting bwa aln sam file to bam and indexing"
-    samtools sort -T ${WORK_DIR}/bamBWA/${baseName}  -@ $nThreads -o ${WORK_DIR}/bamBWA/${baseName}_sort.bam  ${WORK_DIR}/bamBWA/${baseName}.bam
-    samtools index -@ $nThreads ${WORK_DIR}/bamBWA/${baseName}_sort.bam
-    rm ${WORK_DIR}/bamBWA/${baseName}.bam
-    echo "finished repeat alignment"
-
-    #gtfFile=${GENOME_DIR}/annotation/c_elegans.PRJNA13758.${genomeVer}.annotations_rpt.gtf
-    annotFile_rpt=${GENOME_DIR}/annotation/c_elegans.PRJNA13758.${genomeVer}.annotations_rpt.gtf
 
    # count reads per feature with htseq
     mkdir -p ${WORK_DIR}/htseq
-    htseq-count -f bam -r name -a 0 -r pos -m union --nonunique random  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_random.txt
-    htseq-count -f bam -r name -a 0 -r pos -m union --nonunique none  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_none.txt
-#    htseq-count -f bam -r name -a 0 -r pos -m union --nonunique fraction  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_fraction.txt
+    htseq-count -f bam -a 0 -r pos -m union --nonunique random  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_random.txt
+    htseq-count -f bam -a 0 -r pos -m union --nonunique none  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_none.txt
+#    htseq-count -f bam -a 0 -r pos -m union --nonunique fraction  ${WORK_DIR}/bamBWA/${baseName}_sort.bam $annotFile_rpt > ${WORK_DIR}/htseq/${baseName}_union_fraction.txt
 
-fi
+    echo "finished repeat alignment"
+#fi
 
